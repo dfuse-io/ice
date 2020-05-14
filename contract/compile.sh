4c08cf37cef4141d8ba7774804985bddc186ea7c
billettc <charles@cbillette.com>
2020-05-14 12:39:06 -0400

    My fisrt react component that do an infinit loop

--- contract/compile.sh
+++ contract/compile.sh
@@ -11,7 +11,4 @@ CORES=`getconf _NPROCESSORS_ONLN`
 printf "${BROWN}Compiling ${BUILD_SUFFIX}${NC}\n"
 
 mkdir -p $ROOT/build${BUILD_SUFFIX}
-pushd $ROOT/build${BUILD_SUFFIX} &> /dev/null
-cmake ../
-make -j${CORES}
-popd &> /dev/null
+eosio-cpp ./src/ice.cpp -o ./build/ice.wasm
--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,24 +1,138 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+
 import './App.css';
+import {createDfuseClient} from "@dfuse/client"
+
+
+const client = createDfuseClient({
+    apiKey: "web_0123456789abcdef",
+    authUrl: "null://",
+    network: "localhost:13026",
+    secure: false
+});
+
+interface PoolRow {
+    pool_name: string;
+    author: string;
+    description: string
+    ideas: IdeaRow[]
+}
+
+interface IdeaRow {
+    ideaId: number;
+    poolName: string;
+    author: string;
+    description: string;
+    avgImpact: number;
+    avgConfidence: number;
+    avg_ease: number;
+    score: number;
+    total_votes: number;
+}
+
+interface VoteRow {
+    ideaId: number;
+    voter: string;
+    impact: number;
+    confidence: number;
+    ease: number;
+}
+
+const Message = () => {
+    const [message, setMessage] = useState( 'sss' );
+
+    return (
+        <p>
+            <strong>{message}</strong>
+        </p>
+    );
+};
+type IdeaProps = { poolName: string };
+const Idea = ({poolName}: IdeaProps) => {
+    const [ideas, setIdeas] = useState<IdeaRow[]>([]);
+
+    useEffect(() => {
+        client.stateTable<IdeaRow>("dfuseioice", poolName, "ideas")
+            .then((ideaResult) => {
+                console.log("received ideas result");
+                let ideas: IdeaRow[] = [];
+                ideaResult.rows.map(r => {
+                    const idea = r.json!;
+                    console.log("ideas: " + idea.description);
+                    ideas.push(idea);
+                    return true
+                });
+
+                console.log("ideas:" + ideas.length)
+                setIdeas(ideas)
+            })
+            .catch(reason => {
+                console.log(reason)
+
+            });
+    });
+    return (
+        <ul>
+            {ideas.map(i => (
+                <li>{i.description}</li>
+            ))}
+        </ul>
+    )
+}
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          hello world!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [pools, setPools] = useState<PoolRow[]>([]);
+
+    useEffect(() => {
+        try {
+            client.stateTable<PoolRow>("dfuseioice", "dfuseioice", "pools")
+                .then((poolsResult) => {
+
+                    console.log("Creating poolRows");
+                    let poolRows: PoolRow[] = [];
+
+                    poolsResult.rows.map(r => {
+
+                        let pool: PoolRow = r.json!;
+                        console.log("pushing pool: " + pool.pool_name);
+                        poolRows.push(pool);
+
+
+                        return true
+                    });
+                    console.log("Setting poolRows: " + poolRows);
+                    setPools(poolRows)
+                })
+        } catch (e) {
+            console.log("An error occurred", e)
+        }
+        return () => {
+            client.release()
+        }
+    }, []);
+
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Pools</h1>
+                <ul>
+                    {pools.map((p) => (
+                        <div>
+                            <li>
+                                <div>
+                                    <h2>{p.description}({p.pool_name})</h2>
+                                    <Idea poolName={p.pool_name}/>
+                                </div>
+                            </li>
+                        </div>
+                    ))}
+                </ul>
+            </header>
+        </div>
+    );
 }
 
 export default App;
