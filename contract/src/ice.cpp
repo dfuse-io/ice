3bcb772b5e93efdcedf5227cc26f2443e8dc9645
Julien Cassis <juliencassis@gmail.com>
2020-05-13 22:33:16 -0400

    fix contrat

--- contract/src/ice.cpp
+++ contract/src/ice.cpp
@@ -15,19 +15,24 @@ void ice::addpool(const name author,const name name, const string description) {
 }
 
 void ice::addidea(const name author,const name pool_name , const string description) {
-    printf("%s is adding idea %s to pool: %s", author.to_string().c_str(),  description.c_str(), pool_name.to_string().c_str());
-    require_active_auth(author);
-    
-    auto pool_itr = pools.find(pool_name.value);
-    check(pool_itr != pools.end(), "pool " + pool_name.to_string() + " does not exists.");
+  printf("%s is adding idea %s to pool: %s", author.to_string().c_str(),  description.c_str(), pool_name.to_string().c_str());
+  require_active_auth(author);
+  
+  auto pool_itr = pools.find(pool_name.value);
+  check(pool_itr != pools.end(), "pool " + pool_name.to_string() + " does not exists.");
 
-    auto idea_itr = ideas.emplace(_self, [&](auto& idea) {
-      idea.id = ideas.available_primary_key();
-      idea.pool_name = pool_itr->pool_name;
-      idea.author = author;
-      idea.description = description;
-    });
+  auto idea_itr = ideas.emplace(_self, [&](auto& idea) {
+    idea.id = ideas.available_primary_key();
+    idea.pool_name = pool_itr->pool_name;
+    idea.author = author;
+    idea.description = description;
+  });
 }
+
+void ice::castvote(const name voter,const idea_id idea_id, const uint32_t impact,const uint32_t confidence, const uint32_t ease) {
+  printf("%s is voting for %d idea : (%d x %d x %d)", voter.to_string().c_str(), idea_id,impact, confidence, ease); 
+}
+
 
 /**
  * Reset all
@@ -35,9 +40,7 @@ void ice::addidea(const name author,const name pool_name , const string descript
 void ice::reset(const uint64_t any) {
   print("reseting ice tables\n");
   require_active_auth(_self);
-  auto pool_itr = pools.begin();
-  while (pool_itr != pools.end()) {
-      pool_itr = pools.erase(pool_itr);
-    }
-    table_clear(ideas)
+    
+    table_clear(pools);
+    table_clear(ideas);
 }
--- contract/src/ice.hpp
+++ contract/src/ice.hpp
@@ -9,13 +9,15 @@ using std::string;
 
 
 // Typedefs
+typedef uint64_t idea_id;
 
 class [[eosio::contract]] ice : public contract {
   public:
       ice(name receiver, name code, datastream<const char*> ds)
         :contract(receiver, code, ds),
         pools(_self, _self.value),
-        ideas(_self, _self.value) 
+        ideas(_self, _self.value),
+        votes(_self, _self.value)
         {}
               
 
@@ -25,6 +27,9 @@ class [[eosio::contract]] ice : public contract {
       
       [[eosio::action]]
       void addidea(const name author,const name pool_name , const string description);
+      
+      [[eosio::action]]
+      void castvote(const name voter,const idea_id idea_id, const uint32_t impact,const uint32_t confidence, const uint32_t ease);
 
       [[eosio::action]]
       void reset(const uint64_t any);
@@ -46,11 +51,29 @@ class [[eosio::contract]] ice : public contract {
       name pool_name;
       name author;
       string description;
+      float avg_impact;
+      float avg_confidence;
+      float avg_ease;
+      float score;
+      uint64_t total_votes;
       
       uint64_t primary_key() const { return id; }
     };
 
     typedef eosio::multi_index<"ideas"_n, idea> ideas_index;
+
+    struct [[eosio::table]] vote {
+      uint64_t id;
+      name voter;
+      idea_id idea_id;
+      uint32_t impact;
+      uint32_t confidence;
+      uint32_t ease;
+
+      uint64_t primary_key() const { return id; }
+    };
+
+    typedef eosio::multi_index<"votes"_n, vote> votes_index;
 
 
     void require_active_auth(const name account) const { require_auth(permission_level{account, "active"_n}); }
@@ -70,5 +93,6 @@ class [[eosio::contract]] ice : public contract {
 
     pools_index pools;
     ideas_index ideas;
+    votes_index votes;
 };
 
