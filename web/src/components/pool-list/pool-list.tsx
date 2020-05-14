9f70b830bd41ee19995176a0a79bd8ee77158ded
billettc <charles@cbillette.com>
2020-05-14 19:38:09 -0400

    remove dead code

--- web/src/components/pool-list/pool-list.tsx
+++ web/src/components/pool-list/pool-list.tsx
@@ -38,24 +38,6 @@ export const PoolList: React.FC = () => {
     }, [dfuseClient]);
 
 
-    const columns = [
-        {
-            title: 'Name',
-            dataIndex: 'name',
-            key: 'name',
-        },
-        {
-            title: 'Age',
-            dataIndex: 'age',
-            key: 'age',
-        },
-        {
-            title: 'Address',
-            dataIndex: 'address',
-            key: 'address',
-        },
-    ];
-
     return (
         <>
             <Row justify="center">
