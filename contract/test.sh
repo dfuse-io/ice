b3e091d9dfaea9476526c7f322db40c9c09a15de
Josh Kauffman <40213896+joshkauffman@users.noreply.github.com>
2020-05-15 11:56:47 -0400

    Update test.sh

--- contract/test.sh
+++ contract/test.sh
@@ -10,24 +10,24 @@ BROWN='\033[0;33m'
 NC='\033[0m'
 
 pushd $ROOT/vault &> /dev/null
-    printf "${BROWN} create a \'hackthon\' pool ${NC}\n"
+    printf "${BROWN} create a \'hackathon\' pool ${NC}\n"
     eosc tx create dfuseioice addpool '{"author":"babyontheway", "name":"hackathon"}' -p babyontheway
 
     printf "${BROWN} create ideas in the \'hackathon\' pool ${NC}\n"
-    eosc tx create dfuseioice addidea '{"author":"thegreat", "pool_name":"hackathon", "title":"Ship It!", "description":"In computer software - it means to publish / distribute the application in its current state. Often used humurously when the software is buggy and clearly not ready to publish."}' -p thegreat    # transaction_id => 1
-    eosc tx create dfuseioice addidea '{"author":"bigontiktok", "pool_name":"hackathon", "title":"Ship It Now!", "description":"In computer software - it means to publish / distribute the application in its current state. Often used humurously when the software is buggy and clearly not ready to publish. NOW!"}' -p bigontiktok    # transaction_id => 2
-    eosc tx create dfuseioice addidea '{"author":"mohawkbeard", "pool_name":"hackathon", "title":"release dfuse-eosio v1.0.0", "description":"To improve dfuse instrumented nodeos binary processing speed, we had to make incompatible changes to data exchange format going out of nodeos. This requires you to upgrade your dfuse instrumented nodeos binary to latest version "}' -p mohawkbeard    # transaction_id => 3
+    eosc tx create dfuseioice addidea '{"author":"thegreat", "pool_name":"hackathon", "title":"NFT Explorer", "description":"NFTs need an explorer too. So come and explore with me!"}' -p thegreat    # transaction_id => 1
+    eosc tx create dfuseioice addidea '{"author":"bigontiktok", "pool_name":"hackathon", "title":"TikTokChain", "description":"Let's bring blockchain to TikTok. TikTok pre-sale pricing available for all dfuse employees who vote favourably!"}' -p bigontiktok    # transaction_id => 2
+    eosc tx create dfuseioice addidea '{"author":"mohawkbeard", "pool_name":"hackathon", "title":"ICE", "description":"Impact, Confidence, Ease of Implementation. As you can see, it's already implemented, so you should all vote 10!"}' -p mohawkbeard    # transaction_id => 3
 
-    printf "${BROWN} create a \'hackthon2\' pool ${NC}\n"
+    printf "${BROWN} create a \'hacktahon2\' pool ${NC}\n"
     eosc tx create dfuseioice addpool '{"author":"sepedtyepr", "name":"hackathon2"}' -p sepedtyepr
     printf "${BROWN} create ideas in the \'hackathon2\' pool ${NC}\n"
-    eosc tx create dfuseioice addidea '{"author":"weenie", "pool_name":"hackathon2", "title":"This is the best idea!", "description":"In philosophy, ideas are usually taken as mental representational images of some object. Ideas can also be abstract concepts that do not present as mental images. Many philosophers have considered ideas to be a fundamental ontological category of being"}' -p weenie    # transaction_id => 4
-    eosc tx create dfuseioice addidea '{"author":"mrplancher", "pool_name":"hackathon2", "title":"convert flux to grpc..... deng","description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation "}' -p mrplancher    # transaction_id => 5
+    eosc tx create dfuseioice addidea '{"author":"weenie", "pool_name":"hackathon2", "title":"Free coffee", "description":"I'm tired of ordering coffee. Coffee is now a free for all. Don't let the name of the idea confuse you!"}' -p weenie    # transaction_id => 4
+    eosc tx create dfuseioice addidea '{"author":"mrplancher", "pool_name":"hackathon2", "title":"Watcher","description":"My watcher can watch your accounts for you while you watch The Watchmen"}' -p mrplancher    # transaction_id => 5
     for i in  "1","hackathon" "2","hackathon" "3","hackathon" "4","hackathon2" "5","hackathon2"
     do
         IFS=","
         set -- $i
-        for USER in  "thegreat" "babyontheway" "55inchtv" "mrplancher" "reactdev" "weenie" "patch" "sepedtyepr" "mohawkbeard" "bigontiktok" "cno"
+        for USER in  "sepedtyepr" "mohawkbeard"
         do
             IMP="$(( ( RANDOM % 10 )  + 1 ))"
             EAS="$(( ( RANDOM % 10 )  + 1 ))"
