e3a2ed74c4429b10b0d5594244bca7eee2e00d28
billettc <charles@cbillette.com>
2020-05-15 10:01:29 -0400

    added ice chart

--- web/src/components/idea-view/idea-view.tsx
+++ web/src/components/idea-view/idea-view.tsx
@@ -1,50 +1,108 @@
 import React, {useEffect, useState} from 'react';
-import {useAppState} from "../../state"
 import {IdeaRow} from "../../types"
-import {Button, Col, Row, Tooltip, Progress} from 'antd';
+import {Col, Row} from 'antd';
 import {VoteList} from "../vote-list/vote-list";
 import {styled} from "../../theme";
+import {Bar, HorizontalBar} from 'react-chartjs-2';
 
 const Score = styled.span`
     font-size:14px;
     font-weight:bold;
-`
+`;
 const IdeaWrapper = styled.div`
     margin-top: 15px;
     margin-bottom: 5px;
-`
+`;
 const IdeaTitle = styled.h1`
     cursor: pointer;
-`
+`;
 
 const IdeaDescription = styled.p`
     cursor: pointer;
-`
+`;
 
-const eosjsAccountName = require("eosjs-account-name")
+const eosjsAccountName = require("eosjs-account-name");
 
 interface IdeaListProps {
     idea: IdeaRow
 }
 
+
 export const IdeaView: React.FC<IdeaListProps> = ({idea}) => {
 
     const [showForm, setShowForm] = useState<boolean>(false);
+    const chartRef = React.createRef();
 
     const handleClick = () => {
         setShowForm(!showForm)
-    }
+    };
+    const data = {
+        // labels: ['I'],
+        datasets: [
+            {
+                label: 'I',
+                data: [idea.avg_impact],
+                backgroundColor: '#D6E9C6' // green
+            },
+            {
+                label: 'C',
+                data: [idea.avg_confidence],
+                backgroundColor: '#FAEBCC' // yellow
+            },
+            {
+                label: 'E',
+                data: [idea.avg_ease],
+                backgroundColor: '#EBCCD1' // red
+            },
+            {
+                label: 'B',
+                data: [30 - (idea.avg_impact + idea.avg_confidence + idea.avg_ease)],
+                backgroundColor: 'transparent' // red
+            }
+        ]
+    };
+
     return (
         <IdeaWrapper key={idea.id}>
             <Row>
                 <Col span={18}>
-                    <Tooltip title="">
-                        <Progress
-                            percent={idea.avg_confidence * (3.333333)}
-                            successPercent={idea.avg_ease  * (3.333333)}
-                            showInfo={false}
-                        />
-                    </Tooltip>
+                    <HorizontalBar
+                        data={data}
+                        width={100}
+                        height={50}
+
+                        options={{
+                            legend: {
+                                display: false
+
+                            },
+                            maintainAspectRatio: false,
+                            scales: {
+                                xAxes: [{
+                                    suggestedMax: 30,
+                                    stacked: true,
+                                    gridLines : {
+                                        display : false,
+                                        drawTicks: false
+                                    },
+                                    ticks:{
+                                        display : false,
+                                    }
+                                }],
+                                yAxes: [{
+                                    suggestedMax: 30,
+                                    stacked: true,
+                                    gridLines : {
+                                        display : false,
+                                        drawTicks: false
+                                    },
+                                    ticks:{
+                                        display : false,
+                                    }
+                                }]
+                            }
+                        }}
+                    />
                 </Col>
                 <Col span={6} style={{textAlign: "right"}}>
                     <Score>{Math.round(idea.score)}</Score>
