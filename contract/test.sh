728ef26a7b89f613abb6395c37837fa9f516f47c
billettc <charles@cbillette.com>
2020-06-05 07:11:05 -0400

    big clean up

--- contract/test.sh
+++ contract/test.sh
@@ -11,23 +11,23 @@ NC='\033[0m'
 
 pushd $ROOT/vault &> /dev/null
     printf "${BROWN} create a \'hackathon\' pool ${NC}\n"
-    eosc tx create dfuseioice addpool '{"author":"babyontheway", "name":"hackathon"}' -p babyontheway
+    eosc tx create dfuseioice addpool '{"author":"dfuseioice", "name":"hackathon"}' -p dfuseioice
 
     printf "${BROWN} create ideas in the \'hackathon\' pool ${NC}\n"
-    eosc tx create dfuseioice addidea '{"author":"thegreat", "pool_name":"hackathon", "title":"NFT Explorer", "description":"NFTs need an explorer too. So come and explore with me!"}' -p thegreat    # transaction_id => 1
-    eosc tx create dfuseioice addidea '{"author":"bigontiktok", "pool_name":"hackathon", "title":"TikTokChain", "description":"Lets bring blockchain to TikTok. TikTok pre-sale pricing available for all dfuse employees who vote favourably!"}' -p bigontiktok    # transaction_id => 2
-    eosc tx create dfuseioice addidea '{"author":"mohawkbeard", "pool_name":"hackathon", "title":"ICE", "description":"Impact, Confidence, Ease of Implementation. As you can see, its already implemented, so you should all vote 10!"}' -p mohawkbeard    # transaction_id => 3
+    eosc tx create dfuseioice addidea '{"author":"dfuseioice", "pool_name":"hackathon", "title":"NFT Explorer", "description":"NFTs need an explorer too. So come and explore with me!"}' -p dfuseioice    # transaction_id => 1
+    eosc tx create dfuseioice addidea '{"author":"user1", "pool_name":"hackathon", "title":"TikTokChain", "description":"Lets bring blockchain to TikTok. TikTok pre-sale pricing available for all dfuse employees who vote favourably!"}' -p user1    # transaction_id => 2
+    eosc tx create dfuseioice addidea '{"author":"user2", "pool_name":"hackathon", "title":"ICE", "description":"Impact, Confidence, Ease of Implementation. As you can see, its already implemented, so you should all vote 10!"}' -p user2    # transaction_id => 3
 
     printf "${BROWN} create a \'hacktahon2\' pool ${NC}\n"
-    eosc tx create dfuseioice addpool '{"author":"sepedtyepr", "name":"hackathon2"}' -p sepedtyepr
+    eosc tx create dfuseioice addpool '{"author":"dfuseioice", "name":"hackathon2"}' -p dfuseioice
     printf "${BROWN} create ideas in the \'hackathon2\' pool ${NC}\n"
-    eosc tx create dfuseioice addidea '{"author":"weenie", "pool_name":"hackathon2", "title":"Free coffee", "description":"Im tired of ordering coffee. Coffee is now a free for all. Dont let the name of the idea confuse you!"}' -p weenie    # transaction_id => 4
-    eosc tx create dfuseioice addidea '{"author":"mrplancher", "pool_name":"hackathon2", "title":"Watcher","description":"My watcher can watch your accounts for you while you watch The Watchmen"}' -p mrplancher    # transaction_id => 5
+    eosc tx create dfuseioice addidea '{"author":"user1", "pool_name":"hackathon2", "title":"Free coffee", "description":"Im tired of ordering coffee. Coffee is now a free for all. Dont let the name of the idea confuse you!"}' -p user1    # transaction_id => 4
+    eosc tx create dfuseioice addidea '{"author":"user2", "pool_name":"hackathon2", "title":"Watcher","description":"My watcher can watch your accounts for you while you watch The Watchmen"}' -p user2    # transaction_id => 5
     for i in  "1","hackathon" "2","hackathon" "3","hackathon" "4","hackathon2" "5","hackathon2"
     do
         IFS=","
         set -- $i
-        for USER in  "sepedtyepr" "mohawkbeard"
+        for USER in  "user1" "user2"
         do
             IMP="$(( ( RANDOM % 10 )  + 1 ))"
             EAS="$(( ( RANDOM % 10 )  + 1 ))"
